f646200b2e837c4bf73388b7cc0516c5299fe777
Ömer Üstün <omerberatustun@gmail.com>
2023-09-20 20:55:30 +0300

    Enhance token visibility in configuration form
    - Masked the stored DigitalOcean token with asterisks in the configuration form.
    - Adjusted the form to display the exact number of asterisks corresponding to the length of the saved token.
    - Minor template adjustments for improved user experience.
    - Fixed the active tab class setting in 'navbar.html'.

--- config/forms.py
+++ config/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from .models import Configuration
 
 class ConfigurationForm(forms.ModelForm):
-    digitalocean_token = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'DigitalOcean API Token'}))
-    
     class Meta:
         model = Configuration
-        fields = ['digitalocean_token']
+        widgets = {
+            'digitalocean_token': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'DigitalOcean API Token'}),
+
+        }
+        fields = '__all__'
--- config/views.py
+++ config/views.py
@@ -5,12 +5,19 @@ from django.contrib import messages
 
 def config_view(request):
     config, created = Configuration.objects.get_or_create(pk=1)
+    token_placeholder = '*' * len(config.digitalocean_token) if config.digitalocean_token else ''
     if request.method == "POST":
         form = ConfigurationForm(request.POST, instance=config)
         if form.is_valid():
             form.save()
             messages.success(request, "Configuration saved successfully!")
+            return redirect('config_view')
     else:
+        if config.digitalocean_token:
+            visible_chars = config.digitalocean_token[:9]
+            masked_chars = '*' * (len(config.digitalocean_token) - 9)
+            config.digitalocean_token = visible_chars + masked_chars
         form = ConfigurationForm(instance=config)
-    return render(request, 'config.html', {'form': form})
+    return render(request, 'config.html', {'form': form, 'token_placeholder': token_placeholder})
+
 
