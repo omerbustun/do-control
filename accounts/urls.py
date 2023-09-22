9c9384f3357df008788a88c2ef41dc255362bb0e
Ömer Üstün <omerberatustun@gmail.com>
2023-09-22 23:58:03 +0300

    Implement user notifications in navbar
    - Integrated user notifications into the navbar.
    - Displayed the last 5 notifications for the user in a dropdown menu.
    - Added "See all notifications" link in the dropdown to view all notifications.
    - Introduced a badge indicator for unread notifications.
    - Enabled marking individual notifications as read.
    - Handled empty notifications scenario with an appropriate message.
    - Fixed template syntax errors and improved readability.

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(next_page='accounts:login'), name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('notifications/all/', views.all_notifications, name='all_notifications'),
+    path('notifications/mark_read/<int:notification_id>/', views.mark_notification_as_read, name='mark_notification_as_read'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.contrib import messages
+from notifications.models import Notification
+from django.shortcuts import redirect
 
 class CustomLogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
@@ -10,10 +12,21 @@ class CustomLogoutView(LogoutView):
 
 @login_required
 def home_view(request):
-    return render(request, 'dashboard.html')
+    latest_notifications = Notification.objects.filter(recipient=request.user).order_by('-timestamp')[:5]
+    return render(request, 'dashboard.html', {'latest_notifications': latest_notifications})
 
 def base_view(request):
     return render(request, 'base.html')
 
 def profile_view(request):
     return render(request, 'profile.html')
+
+# Notifications
+def all_notifications(request):
+    all_notifications = Notification.objects.filter(recipient=request.user).order_by('-timestamp')
+    return render(request, 'all_notifications.html', {'all_notifications': all_notifications})
+
+def mark_notification_as_read(request, notification_id):
+    notification = Notification.objects.get(id=notification_id, recipient=request.user)
+    notification.mark_as_read()
+    return redirect('all_notifications')
