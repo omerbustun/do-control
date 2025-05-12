142c154cb9fae4fc0f28cd1215b1da50d49da2d0
Ömer Üstün <omerbustun@gmail.com>
2025-05-12 15:02:10 +0300

    chore(repo): clean slate for new implementation

--- config/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-# For keeping the configuration in the DB.
-class Configuration(models.Model):
-    digitalocean_token = models.CharField(max_length=200, null=True, blank=True)
